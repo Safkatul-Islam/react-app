3875a2dee932eb7beede63f6f216a20a0d6e4f63
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-01-28 20:03:22 -0800

    phase-2: state & events with best practices

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,11 +3,21 @@ import { useState } from "react";
 function Counter() {
     const [count, setCount] = useState(0);
 
+    // Best Practice: Define the logic separately. standard industry practice 
+    // is to pull logic out of the JSX return statement. This keeps HTML clean and readable.
+    const handleDecrement = () => {
+        if(count > 0) {
+            setCount(prevCount => prevCount - 1);
+        }
+    };
+
     return (
         <div>
             <h2>Count: {count}</h2>
-            <button onClick={() => setCount(count + 1)}> + </button>
-            <button onClick={() => count > 0 && setCount(count - 1)}> - </button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}> + </button>
+
+            {/* Pass the function reference */}
+            <button onClick={handleDecrement}> - </button>
         </div>
     );
 }
