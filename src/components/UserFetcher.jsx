169f81f22995876023eb0bc3fe25dce8e7ea8a54
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-02-01 21:38:23 -0800

    phase-3.6: useEffect with API with best practices

--- src/components/UserFetcher.jsx
+++ src/components/UserFetcher.jsx
@@ -3,14 +3,24 @@ import { useState, useEffect } from "react";
 function UserFetcher() {
     const [id, setId] = useState(1);
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);  // Start true
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            setUser(data);
-        });
+        // Define async function inside effect
+        const userData = async () => {
+            setLoading(true);  // Start loading
+            try {
+                const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+                const data = response.json();
+                setUser(data);
+            } catch (error) {
+                console.error(`Error fetching data: ${error}`);
+            } finally {
+                setLoading(true);  // Stop loading (success or fail)
+            }
+        };
+
+        userData();
     }, [id]);
 
     /* if (!user) {
@@ -20,9 +30,18 @@ function UserFetcher() {
     return (
         <div>
             <h2>User ID: {id}</h2>
-            <h3>Name: {user?.name || "Loading..."}</h3>
-            <h3>Email: {user?.email || "Loading..."}</h3>
-            <h3>Phone: {user?.phone || "Loading..."}</h3>
+
+            {/* Conditional Rendering: Show ONE loader for the whole block */}
+            {loading ? (
+                <p>Loading user data...</p>
+            ) : (
+                <>
+                    <h3>Name: {user.name}</h3>
+                    <h3>Email: {user.email}</h3>
+                    <h3>Phone: {user.phone}</h3>
+                </>
+            )}
+
             <button onClick={() => setId(id + 1)}>Next User (+)</button>
             <button onClick={() => id > 1 && setId(id - 1)}>Prev User (-)</button>
         </div>
