8cf439b3cad08f44e9ea5124fd900ecb896b8b55
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-02-01 18:39:11 -0800

    phase-3.6: useEffect with API and handling loading state

--- src/components/UserFetcher.jsx
+++ src/components/UserFetcher.jsx
@@ -2,20 +2,27 @@ import { useState, useEffect } from "react";
 
 function UserFetcher() {
     const [id, setId] = useState(1);
-    const [user, setUser] = useState("Loading...");
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
-        console.log("Fetching data for user: " + id);
-        setUser("Loading...");
-        setTimeout(() => {
-            setUser("Name for user " + id);
-        }, 1000)
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setUser(data);
+        });
     }, [id]);
 
+    /* if (!user) {
+        return <div>Loading...</div>
+    } */
+    
     return (
         <div>
-            <h1>User ID: {id}</h1>
-            <h2>Name: {user}</h2>
+            <h2>User ID: {id}</h2>
+            <h3>Name: {user?.name || "Loading..."}</h3>
+            <h3>Email: {user?.email || "Loading..."}</h3>
+            <h3>Phone: {user?.phone || "Loading..."}</h3>
             <button onClick={() => setId(id + 1)}>Next User (+)</button>
             <button onClick={() => id > 1 && setId(id - 1)}>Prev User (-)</button>
         </div>
